edd5423e765a4988a702e12bf25def72086b431a
igor2000xp <igorring@google.com>
2026-01-16 07:32:10 +0100

    refactor: improve code organization for auth system
    - Extract DI registration into extension methods
    - Organize Program.cs with clear sections
    - Improve separation of concerns between layers

--- src/BlogPlatform.Application/obj/Debug/net10.0/BlogPlatform.Application.AssemblyInfo.cs
+++ src/BlogPlatform.Application/obj/Debug/net10.0/BlogPlatform.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogPlatform.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7a2b1434f97749f5b10d3deda0edad9a77b6fbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f2aabf7aca5b302915fa767375299ba3367136")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogPlatform.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogPlatform.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
